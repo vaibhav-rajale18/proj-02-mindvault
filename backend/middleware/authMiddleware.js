5720a17331d12901f6d55c60bdd485e08959fec4
vaibhav-rajale18 <vaibhavrajale2005@gmail.com>
2026-06-05 17:16:38 +0530

    fix: resolve protected entries authentication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,26 +1,36 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+const protect = async (req, res, next) => {
+let token;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({
-      message: "Authorization token missing",
-    });
-  }
+if (
+req.headers.authorization &&
+req.headers.authorization.startsWith("Bearer")
+) {
+try {
+token = req.headers.authorization.split(" ")[1];
 
-  const token = authHeader.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded.id;
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      message: "Invalid or expired token",
-    });
-  }
+  req.user = decoded.id;
+
+  next();
+} catch (error) {
+  console.error(error);
+  return res.status(401).json({
+    message: "Not authorized, token failed",
+  });
+}
+
+
+}
+
+if (!token) {
+return res.status(401).json({
+message: "Not authorized, no token",
+});
+}
 };
 
-module.exports = authMiddleware;
+module.exports = protect;
