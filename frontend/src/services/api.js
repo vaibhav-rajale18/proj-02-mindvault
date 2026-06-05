c311ee391171d32e9588462a613d98ea308c2515
vaibhav-rajale18 <vaibhavrajale2005@gmail.com>
2026-06-05 14:18:38 +0530

    fix: update frontend production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+const API = axios.create({
+baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("mindvault_token");
+API.interceptors.request.use((req) => {
+const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+if (token) {
+req.headers.Authorization = `Bearer ${token}`;
+}
 
-  return config;
+return req;
 });
 
-export default api;
+export default API;
